14f72c395c4d5aa496430284c70df40642e4a4f4
AndreeaG <andreea.g2001@yahoo.com>
2025-05-29 16:09:05 +0300

    CORS configuration

--- myhome-backend/index.js
+++ myhome-backend/index.js
@@ -1,14 +1,20 @@
 const express = require('express');
+const app = express();
 const mongoose = require('mongoose');
 const cors = require('cors');
 const bcrypt = require('bcrypt');
 require('dotenv').config();
 
-const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
-app.use(cors());
+//app.use(cors());
+app.use(cors({
+  origin: ['https://myhomedesigner.xyz', 'https://www.myhomedesigner.xyz'], 
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
 app.use(express.json());
 
 const authRoutes = require('./routes/auth');
